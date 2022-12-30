1ec6ed0e49682803041648d53afefb9a394df035
zhang <8804991+zhangtuo0723@user.noreply.gitee.com>
2022-12-30 17:02:06 +0800

    暴露项目配置项

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.scss'
+// import './App.scss'
 import { Button, Space } from 'antd-mobile'
 export default function App() {
-  return <div className='aa'>
-    <Button color='primary' fill='solid'>
+  return <div className='aa' style={{width:'100px',height:'100px',backgroundColor:'pink'}}>
+    <Button color='primary' fill='solid' >
             Solid
           </Button>
   </div>
