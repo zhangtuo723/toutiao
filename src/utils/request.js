12be912333bd2b8de54b57cf6ae7257f25845723
zhang <8804991+zhangtuo0723@user.noreply.gitee.com>
2023-01-15 12:10:41 +0800

    无感刷新token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,47 +1,85 @@
-import axios from 'axios'
-import {Toast}from 'antd-mobile'
-import { getTokenInfo } from './storage'
-import history from '@/history'
+import axios from "axios";
+import { Toast } from "antd-mobile";
+import { getTokenInfo, setTokenInfo } from "./storage";
+import history from "@/history";
+import store from "@/store";
+import { saveToken } from "@/store/action/login";
+import { logout } from "@/store/action/profile";
+const baseUrl = "http://geek.itheima.net/v1_0"
 // 1. 创建新的 axios 实例
 const http = axios.create({
-  baseURL: 'http://geek.itheima.net/v1_0',
-  timeout:5000
-})
+  baseURL: baseUrl,
+  timeout: 5000,
+});
 
 // 2. 设置请求拦截器和响应拦截器
-http.interceptors.request.use(config => {
-
-  const token = getTokenInfo().token
-  if(token){
-    config.headers['Authorization'] = 'Bearer '+token
+http.interceptors.request.use((config) => {
+  const token = getTokenInfo().token;
+  if (token) {
+    config.headers["Authorization"] = "Bearer " + token;
   }
-  
-  return config
-})
 
-http.interceptors.response.use(response => {
-  return response.data
-}, error => {
-  // console.log(error);
+  return config;
+});
 
-  if(error.response){
-    Toast.show({
-      content:error.response.data.message
-    })
-    if(error.response.status===401){
-      // console.log('回退登录页面');
-      history.push('/login',{pathfrom:history.location})
-      
+http.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  async (error) => {
+    // console.log(error);
+
+    
+    if(!error.response){
+      Toast.show({
+        content: "网络繁忙，请稍后重试",
+      });
+      return Promise.reject(error);
     }
 
-  }else{
-    Toast.show({
-      content:"网络繁忙，请稍后重试"
-    })
+    if(error.response.status !== 401){
+      Toast.show({
+        content: error.response.data.message,
+      });
+      return Promise.reject(error);
+    }
+
+    // 401 错误
+    const {refresh_token} = getTokenInfo()
+    // 没有刷新token就去登录
+    if(!refresh_token){
+      history.replace('/login',{pathfrom:history.location.pathname})
+      return Promise.reject(error);
+    }
+    // 有刷新token，发请求，但是不能使用当前的这个http实例,直接用axios
+
+    try {
+      const res= await axios({
+        method:'put',
+        url: baseUrl+'/authorizations',
+        headers:{Authorization:'Bearer '+ refresh_token}
+      })
+     
+      const tokenInfo = {token:res.data.data.token,refresh_token}
+      // 保存redux
+      store.dispatch(saveToken(tokenInfo))
+      // 保存本地
+      setTokenInfo(tokenInfo)
+      // token 刷新后需要重新发送一次之前的请求 error里面的config 就是请求的参数
+      // 这时候就不返回错误了
+      return http(error.config)
+
+
+    } catch (error) {
+      store.dispatch(logout())
+      history.replace('/login',{pathfrom:history.location.pathname})
+      return Promise.reject(error)
+    }
+
+
+    
   }
-  
-  return Promise.reject(error)
-})
+);
 
 // 3. 导出该 axios 实例
-export default http
+export default http;
