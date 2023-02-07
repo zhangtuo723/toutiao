9830166b71ea5da02f65e6d98816b5a168d94278
zhang <8804991+zhangtuo0723@user.noreply.gitee.com>
2023-02-07 13:35:25 +0800

    ts-改造storage

---
+++ src/utils/storage.ts
@@ -0,0 +1,49 @@
+// 用户 Token 的本地缓存键名
+const TOKEN_KEY = "geek-itcast";
+const CHANNELS_KEY = "geek-itcast-channels";
+
+type Token = {
+  token: string;
+  refresh_token: string;
+};
+/**
+ * 从本地缓存中获取 Token 信息
+ */
+export const getTokenInfo = () => {
+  return JSON.parse(localStorage.getItem(TOKEN_KEY) || '{}');
+};
+
+/**
+ * 将 Token 信息存入缓存
+ * @param {Object} tokenInfo 从后端获取到的 Token 信息
+ */
+export const setTokenInfo = (tokenInfo: Token) => {
+  localStorage.setItem(TOKEN_KEY, JSON.stringify(tokenInfo));
+};
+
+/**
+ * 删除本地缓存中的 Token 信息
+ */
+export const removeTokenInfo = () => {
+  localStorage.removeItem(TOKEN_KEY);
+};
+
+/**
+ * 判断本地缓存中是否存在 Token 信息
+ */
+export const hasToken = () => {
+  return !!getTokenInfo().token;
+};
+
+export const seLocalChannels = (channels:Object[]) => {
+  localStorage.setItem(CHANNELS_KEY, JSON.stringify(channels));
+};
+
+export const getLocalChannels = ():Token=> {
+  return JSON.parse(localStorage.getItem(CHANNELS_KEY) as string);
+};
+
+
+export const removeLocalChannels = () => {
+  localStorage.removeItem(CHANNELS_KEY);
+};
